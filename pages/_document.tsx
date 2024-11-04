d60dc967ac9209a80f664d24859dd5cea084f0c9
Aidan Baker <baker339@purdue.edu>
2024-11-04 16:46:07 -0500

    fixed head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,14 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <Script
-        async
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
-        strategy="lazyOnload"
-        crossOrigin="anonymous"
-      />
+      <Head>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
