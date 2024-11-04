44ca02b0d4f07df32498c6e6113b2d763ad367ff
Aidan Baker <baker339@purdue.edu>
2024-11-04 16:38:57 -0500

    added adsense script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+        strategy="lazyOnload"
+        crossOrigin="anonymous"
+      />
       <body className="antialiased">
         <Main />
         <NextScript />
